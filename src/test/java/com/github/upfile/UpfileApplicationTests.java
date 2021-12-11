0dd9e4e61d5bdce4c5d887e0ad52be095923d011
crazydev <mlodyv111@gmail.com>
2021-12-11 17:43:55 +0100

    project init

---
+++ src/test/java/com/github/upfile/UpfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.upfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
